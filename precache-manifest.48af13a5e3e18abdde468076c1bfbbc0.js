c036ae20d88eaf3e23803fa0f1b9a8ead4f8e803
DimaAdamovich <dima94adamovich@gmail.com>
2020-04-20 12:35:26 +0300

    Updates

---
+++ precache-manifest.48af13a5e3e18abdde468076c1bfbbc0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e32a1ac84ef6c3f4e8c8537440cbe76e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d914e4daae55beddf94e",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "a63498524caf13e58988",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "d914e4daae55beddf94e",
+    "url": "./static/js/2.e733f577.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.e733f577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a63498524caf13e58988",
+    "url": "./static/js/main.f69769cd.chunk.js"
+  },
+  {
+    "revision": "7e1daf912dc12ec89125",
+    "url": "./static/js/runtime-main.bcd51850.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
